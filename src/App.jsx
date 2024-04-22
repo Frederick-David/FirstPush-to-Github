0050cb34c58c3a8a354bfda5f4ae031b4912ebbf
FrederickDavid <muomaifefrederick61@gmail.com>
2024-04-22 12:04:45 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled"
+
+function App() {
+  const name = "Frederick David"
+
+  return (
+    <Fred>
+      Hello {name}
+      <Wrapper>Me</Wrapper>
+      <Image />
+      <Input placeholder="Enter name" />
+      <Button>Submit</Button>
+    </Fred>
+  )
+}
+
+export default App
+
+const Button = styled.button``
+
+const Input = styled.input``
+const Image = styled.img``
+
+const Wrapper = styled.div``
+
+const Fred = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: lightgray;
+  color: black;
+  font-size: 40px;
+`
